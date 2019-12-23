c73191664257149afa43551fd639886d0c2ad75f
leanaAlq <leana.a.q@gmail.com>
2019-12-23 22:40:22 +0300

    Updates

---
+++ precache-manifest.bb5fc1a61aa0d90879bb8e0aa7264a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7490901c1e1ae71bbf7a7ef9c6ecfbe5",
+    "url": "/leanaAlq/where-to-eat.git/index.html"
+  },
+  {
+    "revision": "1013b1471ed123ea02c1",
+    "url": "/leanaAlq/where-to-eat.git/static/css/main.2deca179.chunk.css"
+  },
+  {
+    "revision": "99c5430df0ef0e824529",
+    "url": "/leanaAlq/where-to-eat.git/static/js/2.9929e63f.chunk.js"
+  },
+  {
+    "revision": "a3386e1ad3bc53f145c2af54fb4e905f",
+    "url": "/leanaAlq/where-to-eat.git/static/js/2.9929e63f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1013b1471ed123ea02c1",
+    "url": "/leanaAlq/where-to-eat.git/static/js/main.d37de52e.chunk.js"
+  },
+  {
+    "revision": "2ce7292d13df0e0c8c41",
+    "url": "/leanaAlq/where-to-eat.git/static/js/runtime-main.8d6f7cfc.js"
+  },
+  {
+    "revision": "a9a75cddb6b73882512f3fa7bae5b91d",
+    "url": "/leanaAlq/where-to-eat.git/static/media/background.a9a75cdd.jpg"
+  }
+]);
